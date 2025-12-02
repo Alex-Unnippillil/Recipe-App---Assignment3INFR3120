e55da9379375ba7d66281de16f3056962258f0c2
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-12-02 10:33:10 +0000

    Add main Express server

---
+++ recipe-app/app.js
@@ -0,0 +1,43 @@
+// app.js
+
+// load the modules
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// load environment variables
+dotenv.config();
+
+// Import the Recipe routes module
+const recipeRoutes = require('./routes/recipes');
+
+// create an express application
+const app = express();
+
+// Configure view engine as EJStemplating
+app.set('view engine', 'ejs');
+
+// Middleware parse incoming form data
+app.use(express.urlencoded({ extended: true }));
+
+
+// Serve static files from the public directory / css  , images, js
+app.use(express.static('public'));
+
+const mongoURI = process.env.MONGODB_URI;
+if (!mongoURI) {
+    console.error("Error: MONGODB_URI not set ");
+    process.exit(1);  // Exit if no connection 
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB database"))
+    .catch(err => {
+        console.error("Failed to connect to MongoDB:", err);
+        process.exit(1);
+    });
+app.use('/recipes', recipeRoutes);
+
+app.listen(PORT, () => {
+    console.log(` Server started - port:${PORT}. http://localhost:${PORT}/`);
+});
